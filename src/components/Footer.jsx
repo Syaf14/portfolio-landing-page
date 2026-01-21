ea42ec40f62cc3fde50df7996f347a6eb792a348
Wan Mohammad Syafrul Aiman <syafaiman0135@gmail.com>
2026-01-21 22:28:57 +0800

    add bootstrap js

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 const Footer = () => {
   return (
     <footer className="footer" style={{background:"#202A66"}}>
-      <p className='fw-bolder'>&copy; 2026 My Company. All rights reserved.</p>
+      <p className='fw-bolder text-center'>&copy; 2026 My Company. All rights reserved.</p>
     </footer>
   );
 };
